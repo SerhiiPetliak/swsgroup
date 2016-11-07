5c7ec8dccf1cf9ef05869d4056c5ff85632c56f0
DESKTOP-J9LBAEI\petly <petliak.serhii@gmail.com>
2016-11-07 19:51:16 +0200

    fix

--- app/Http/Controllers/WorksController.php
+++ app/Http/Controllers/WorksController.php
@@ -9,15 +9,6 @@ use Illuminate\Support\Facades\Auth;
 class WorksController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
