b22014f954b6f6a97c440fb17ca33e08ba1c2e3b
DESKTOP-J9LBAEI\petly <petliak.serhii@gmail.com>
2016-11-08 19:04:51 +0200

    add works db seeder

---
+++ database/seeds/WorksSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Works;
+
+class WorksSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('works')->truncate();
+
+        Works::insert([
+            [
+                'title'      => 'Audio',
+                'description' => 'Описание',
+                'preview_img' => 'user5821fe8bd8dc19.95109455.jpg',
+                'img' => 'user5821e66eefc194.12641625.jpg'
+            ],
+            [
+                'title'      => 'Плацдарм',
+                'description' => 'Описание',
+                'preview_img' => 'user5821fdf7774749.04592012.jpg',
+                'img' => 'user5821e741e5bf01.60342964.jpg'
+            ],
+            [
+                'title'      => 'Мед. учет',
+                'description' => 'ИС медицинского учета разработана для выполнения следующих задач:<ul><li>внесения и хранения информации о жителях  медицинского участка</li><li>составления списков жителей по отдельным улицам</li><li>контроля своевременного прохождения флюорографии</li></ul> Для разработки системы использованы следующие технологии:<ul> <li>PHP Framework <strong>Yii2</strong></li><li>БД - <strong>MySQL</strong></li></ul>',
+                'preview_img' => 'user5821f9ba6a46f9.53134588.jpg',
+                'img' => 'user5821e7a3d4e9a1.48820050.jpg'
+            ],
+            [
+                'title'      => 'Incube',
+                'description' => 'Описание',
+                'preview_img' => 'user5821f83a965588.05414467.jpg',
+                'img' => 'user5821e7dbaa6044.94678107.jpg'
+            ],
+            [
+                'title'      => 'SutkiHouse',
+                'description' => 'Описание',
+                'preview_img' => 'user5821f6c4589311.04199017.jpg',
+                'img' => 'user5821e8076afbc8.57259228.jpg'
+            ],
+        ]);
+    }
+}
